fd19841f7d9a5396ec88cbe852001ba7f426363b
Alessandro Di Maria <adm@m42.ch>
2018-10-11 09:46:54 +0200

    counter page

--- apps/thunder_phoenix/lib/thunder_phoenix_web/controllers/page_controller.ex
+++ apps/thunder_phoenix/lib/thunder_phoenix_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule ThunderPhoenixWeb.PageController do
   use ThunderPhoenixWeb, :controller
 
   def index(conn, _params) do
-    render conn, "index.html"
+    render(conn, "index.html")
+  end
+
+  def counter(conn, _params) do
+    render(conn, "counter.html")
   end
 end
--- apps/thunder_phoenix/lib/thunder_phoenix_web/router.ex
+++ apps/thunder_phoenix/lib/thunder_phoenix_web/router.ex
@@ -2,21 +2,23 @@ defmodule ThunderPhoenixWeb.Router do
   use ThunderPhoenixWeb, :router
 
   pipeline :browser do
-    plug :accepts, ["html"]
-    plug :fetch_session
-    plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    plug(:accepts, ["html"])
+    plug(:fetch_session)
+    plug(:fetch_flash)
+    plug(:protect_from_forgery)
+    plug(:put_secure_browser_headers)
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug(:accepts, ["json"])
   end
 
   scope "/", ThunderPhoenixWeb do
-    pipe_through :browser # Use the default browser stack
+    # Use the default browser stack
+    pipe_through(:browser)
 
-    get "/", PageController, :index
+    get("/", PageController, :index)
+    get("/counter", PageController, :counter)
   end
 
   # Other scopes may use custom stacks.
