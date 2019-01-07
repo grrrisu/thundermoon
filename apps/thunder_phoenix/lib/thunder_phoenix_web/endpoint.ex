64fbde0846111d1302adbe51ee9453726793c1c7
Alessandro Di Maria <adm@m42.ch>
2019-01-07 15:09:25 +0100

    replace Poison with Jason for json parsing

--- apps/thunder_phoenix/lib/thunder_phoenix_web/endpoint.ex
+++ apps/thunder_phoenix/lib/thunder_phoenix_web/endpoint.ex
@@ -27,7 +27,7 @@ defmodule ThunderPhoenixWeb.Endpoint do
   plug(Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Poison
+    json_decoder: Jason
   )
 
   plug(Plug.MethodOverride)
